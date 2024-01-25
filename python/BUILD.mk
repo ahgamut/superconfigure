b0d57405e099cc36d51d7ed4736872a546ff1f6b
Gautham <ahgamut@gmail.com>
2024-01-25 13:43:23 +0000

    add scrapscript recipe

--- python/BUILD.mk
+++ python/BUILD.mk
@@ -2,6 +2,7 @@
 include python/cpy311-datasette/BUILD.mk
 include python/cpy311-pypack1/BUILD.mk
 include python/cpy311-pypack2/BUILD.mk
+include python/cpy311-scrapscript/BUILD.mk
 
 all-python: datasette pypack1 pypack2
 .PHONY: all-python
---
+++ python/cpy311-scrapscript/BUILD.mk
@@ -0,0 +1,27 @@
+
+SCRAPSCRIPT_DEPS := lib/ncurses lib/readline lib/gdbm \
+				  lib/libexpat lib/libuuid \
+				  cosmo-repo/compress cosmo-repo/base compress/xz
+
+$(eval $(call SPECIFY_DEPS,python/cpy311-scrapscript,$(SCRAPSCRIPT_DEPS)))
+
+o/python/cpy311-scrapscript/downloaded: \
+	DL_COMMAND = rm -rf ./scrapscript && \
+		git clone --quiet --depth=1 --branch=scrapscript https://github.com/ahgamut/cpython scrapscript
+
+o/python/cpy311-scrapscript/patched: \
+	PATCH_COMMAND = $(DUMMYLINK0)
+
+o/python/cpy311-scrapscript/configured.x86_64: CONFIG_COMMAND = $(BASELOC)/python/cpy311-scrapscript/config-wrapper
+o/python/cpy311-scrapscript/configured.aarch64: CONFIG_COMMAND = $(BASELOC)/python/cpy311-scrapscript/config-wrapper
+
+o/python/cpy311-scrapscript/built.x86_64: BUILD_COMMAND = $(DUMMYLINK0)
+o/python/cpy311-scrapscript/built.aarch64: BUILD_COMMAND = $(DUMMYLINK0)
+
+o/python/cpy311-scrapscript/installed.x86_64: INSTALL_COMMAND = $(DUMMYLINK0)
+o/python/cpy311-scrapscript/installed.aarch64: INSTALL_COMMAND = $(DUMMYLINK0)
+
+o/python/cpy311-scrapscript/built.fat: FATTEN_COMMAND = $(BASELOC)/python/cpy311-scrapscript/fatten
+
+scrapscript: o/python/cpy311-scrapscript/built.fat
+.PHONY: scrapscript
