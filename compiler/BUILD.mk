2672562e4fbd9a71baecaf0db68c4464062a8d9c
Gautham <ahgamut@gmail.com>
2024-02-11 18:49:30 +0000

    use binutils-2.42 and gcc-12.3
    gcc-12.3 builds via the following github branch:
    
    https://github.com/ahgamut/gcc/tree/portcosmo-12.3
    
    but it is also possible to download gcc-12.3 sources and then use a diff
    based on the above branch.

--- compiler/BUILD.mk
+++ compiler/BUILD.mk
@@ -1,6 +1,6 @@
 
 include compiler/binutils/BUILD.mk
-include compiler/gcc-11.2-patched/BUILD.mk
+include compiler/gcc-12.3-patched/BUILD.mk
 include compiler/llvm/BUILD.mk
 
 cosmocc: x86_64-gcc aarch64-gcc
