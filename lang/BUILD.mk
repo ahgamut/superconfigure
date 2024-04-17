b4dfeadb6c653fd39ffdd7d8cd9794407ddb0567
Gautham <ahgamut@gmail.com>
2024-04-17 01:53:01 +0000

    add minimal php build

--- lang/BUILD.mk
+++ lang/BUILD.mk
@@ -2,10 +2,12 @@
 include lang/berry/BUILD.mk
 include lang/janet/BUILD.mk
 include lang/lua/BUILD.mk
+include lang/php/BUILD.mk
 
 lang: \
 	o/lang/berry/built.fat \
 	o/lang/janet/built.fat \
-	o/lang/lua/built.fat
+	o/lang/lua/built.fat \
+	o/lang/php/built.fat
 
 .PHONY: lang
