5d680489c8cf37ece29326d72539d6f642a8230c
Gautham <ahgamut@gmail.com>
2024-04-19 23:09:19 +0000

    add tclsh8.6

---
+++ lang/tcl/BUILD.mk
@@ -0,0 +1,16 @@
+
+TCL_SRC := https://github.com/tcltk/tcl/archive/refs/tags/core-8-6-14.tar.gz
+
+TCL_CONFIG_ARGS = --prefix=$(COSMOS) --disable-shared \
+				  --enable-static --enable-threads \
+				  --disable-rpath --disable-load \
+				  --enable--symbols --disable-langinfo \
+				  --disable-dll-unloading
+
+$(eval $(call DOWNLOAD_SOURCE,lang/tcl,$(TCL_SRC)))
+
+o/lang/tcl/configured.x86_64: CONFIG_COMMAND = $(BASELOC)/o/lang/tcl/tcl*/unix/configure $(TCL_CONFIG_ARGS)
+o/lang/tcl/configured.aarch64: CONFIG_COMMAND = $(BASELOC)/o/lang/tcl/tcl*/unix/configure $(TCL_CONFIG_ARGS)
+
+o/lang/tcl/built.fat: FATTEN_COMMAND = $(BASELOC)/config/apelink_folder.sh
+o/lang/tcl/built.fat: BINS = tclsh8.6 tcl tcl8
