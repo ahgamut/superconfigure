ac5b734aba0c2ad46bff328b0c88741e1f327e6c
Gautham <ahgamut@gmail.com>
2024-04-19 23:00:55 -0500

    one more emacs fix

--- editor/emacs/BUILD.mk
+++ editor/emacs/BUILD.mk
@@ -12,5 +12,5 @@ o/editor/emacs/configured.x86_64:\
 o/editor/emacs/configured.aarch64:\
 	CONFIG_COMMAND = $(BASELOC)/editor/emacs/config-wrapper
 
-o/editor/emacs/built.aarch64: o/editor/emacs/built.x86_64
+o/editor/emacs/configured.aarch64: o/editor/emacs/built.x86_64
 o/editor/emacs/built.fat: FATTEN_COMMAND = $(BASELOC)/editor/emacs/fatten
