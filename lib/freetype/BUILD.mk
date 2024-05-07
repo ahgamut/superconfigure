0826574dc187a09564f458c952978205399a88cd
Gautham <ahgamut@gmail.com>
2024-05-07 13:36:53 +0000

    get freetype and fribidi to build
    freetype needs zip from cosmo-repo/base
    fribidi manpages are skipped

--- lib/freetype/BUILD.mk
+++ lib/freetype/BUILD.mk
@@ -1,6 +1,7 @@
 
 # Documentation: https://gitlab.freedesktop.org/freetype/freetype
 FREETYPE_SRC := https://download.savannah.gnu.org/releases/freetype/freetype-2.13.2.tar.gz
+FREETYPE_DEPS := cosmo-repo/base
 
 FREETYPE_CONFIG_ARGS = --enable-static --disable-shared --with-tests=no\
     --with-brotli=no --with-harfbuzz=no --with-png=no --with-librsvg=no\
@@ -8,6 +9,7 @@ FREETYPE_CONFIG_ARGS = --enable-static --disable-shared --with-tests=no\
     CFLAGS="-Os"
 
 $(eval $(call DOWNLOAD_SOURCE,lib/freetype,$(FREETYPE_SRC)))
+$(eval $(call SPECIFY_DEPS,lib/freetype,$(FREETYPE_DEPS)))
 $(eval $(call AUTOTOOLS_BUILD,lib/freetype,$(FREETYPE_CONFIG_ARGS),$(FREETYPE_CONFIG_ARGS)))
 
 o/lib/freetype/built.fat: FATTEN_COMMAND = $(DUMMYLINK0)
--- lib/fribidi/BUILD.mk
+++ lib/fribidi/BUILD.mk
@@ -1,14 +1,17 @@
 
-FRIBIDI_SRC := https://github.com/fribidi/fribidi/archive/refs/tags/v1.0.13.tar.gz
+FRIBIDI_SRC := https://github.com/fribidi/fribidi/archive/refs/tags/v1.0.14.tar.gz
 
 $(eval $(call DOWNLOAD_SOURCE,lib/fribidi,$(FRIBIDI_SRC)))
 
 FRIBIDI_CONFIG_ARGS = --disable-shared --enable-static \
+	--disable-docs \
     --without-pic --with-gnu-ld --prefix=$(COSMOS) CFLAGS="-Os"
 
 o/lib/fribidi/configured.x86_64: CONFIG_COMMAND = \
-	$(BASELOC)/o/lib/fribidi/fribidi-*/autogen.sh $(FRIBIDI_CONFIG_ARGS)
+	cp -r $(BASELOC)/o/lib/fribidi/fribidi-*/* ./ && \
+		./autogen.sh $(FRIBIDI_CONFIG_ARGS)
 o/lib/fribidi/configured.aarch64: CONFIG_COMMAND = \
-	$(BASELOC)/o/lib/fribidi/fribidi-*/autogen.sh $(FRIBIDI_CONFIG_ARGS)
+	cp -r $(BASELOC)/o/lib/fribidi/fribidi-*/* ./ && \
+		./autogen.sh $(FRIBIDI_CONFIG_ARGS)
 
 o/lib/fribidi/built.fat: FATTEN_COMMAND = $(DUMMYLINK0)
