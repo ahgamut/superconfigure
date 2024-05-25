25f508e395af14042806d2175705ce9e7492201d
Gautham <ahgamut@gmail.com>
2024-05-25 09:11:20 -0500

    disable openssh and rsync builds for now

--- cosmo-repo/cosmos.mk
+++ cosmo-repo/cosmos.mk
@@ -3,7 +3,6 @@ cosmos: \
 	cli lang compress datasette pypack1 editor \
 	o/web/links/built.fat \
 	o/web/wget/built.fat \
-	o/web/rsync/built.fat \
 	o/web/curl/built.fat
 
 .PHONY: cosmos
--- web/BUILD.mk
+++ web/BUILD.mk
@@ -1,17 +1,15 @@
 
-include web/wget/BUILD.mk
 include web/curl/BUILD.mk
+include web/git/BUILD.mk
 include web/links/BUILD.mk
 include web/openssh/BUILD.mk
-include web/git/BUILD.mk
 include web/rsync/BUILD.mk
+include web/wget/BUILD.mk
 
 web:\
-	o/web/wget/built.fat \
 	o/web/curl/built.fat \
-	o/web/links/built.fat \
 	o/web/git/built.fat \
-	o/web/openssh/built.fat \
-	o/web/rsync/built.fat
+	o/web/links/built.fat \
+	o/web/wget/built.fat
 
 .PHONY: web
