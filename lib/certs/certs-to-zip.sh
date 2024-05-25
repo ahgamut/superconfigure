796418f46e479f46790da8378a3c0e8600e125ce
Gautham <ahgamut@gmail.com>
2024-05-25 14:51:53 +0000

    adjust ssl path in /zip

--- lib/certs/certs-to-zip.sh
+++ lib/certs/certs-to-zip.sh
@@ -3,11 +3,11 @@ set -e
 
 # the zip folder
 cd /zip
-mkdir -p ./usr/share/ssl/root
-cp /etc/ssl/certs/*.* ./usr/share/ssl/root/ || true
+mkdir -p ./usr/share/ssl/certs
+cp /etc/ssl/certs/*.* ./usr/share/ssl/certs || true
 
-mkdir -p ./ssl/
-cp -r /etc/ssl/certs ./ssl
+mkdir -p ./ssl/certs
+cp /etc/ssl/certs/*.* ./ssl
 
 cp "$BASELOC"/web/wget/wgetrc /zip
 ls -al /zip
