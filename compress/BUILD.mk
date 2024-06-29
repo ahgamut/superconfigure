9325fa28f8bcde939fb6e633c9b02f840e3a239d
Gautham <ahgamut@gmail.com>
2024-06-29 15:24:15 -0500

    skip lzip build for now
    the downloader is failing

--- compress/BUILD.mk
+++ compress/BUILD.mk
@@ -1,10 +1,10 @@
 
 include compress/brotli/BUILD.mk
 include compress/gzip/BUILD.mk
-include compress/lunzip/BUILD.mk
+# include compress/lunzip/BUILD.mk
 include compress/lz4/BUILD.mk
-include compress/lzlib/BUILD.mk
-include compress/lzip/BUILD.mk
+# include compress/lzlib/BUILD.mk
+# include compress/lzip/BUILD.mk
 include compress/pigz/BUILD.mk
 include compress/tar/BUILD.mk
 
@@ -12,11 +12,9 @@ compress: \
 	o/cosmo-repo/compress/built.fat\
 	o/compress/brotli/built.fat\
 	o/compress/gzip/built.fat\
-	o/compress/lz4/built.fat\
-	o/compress/lzip/built.fat\
 	o/compress/pigz/built.fat\
 	o/compress/tar/built.fat
 
-# tar build doesn't work for some reason
+# lzip build doesn't work for some reason
 
 .PHONY: compress
