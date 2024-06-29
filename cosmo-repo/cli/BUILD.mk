0b1532b4c82b1155398123d60e744f883cb37061
Gautham <ahgamut@gmail.com>
2024-06-29 15:13:38 -0500

    build x86_64 and aarch64 one after the other
    cosmo-repo build is fast enough

--- cosmo-repo/cli/BUILD.mk
+++ cosmo-repo/cli/BUILD.mk
@@ -8,6 +8,8 @@ o/cosmo-repo/cli/configured.aarch64: CONFIG_COMMAND = $(DUMMYLINK0)
 o/cosmo-repo/cli/built.x86_64: BUILD_COMMAND = $(BASELOC)/cosmo-repo/cli/build-wrapper
 o/cosmo-repo/cli/built.aarch64: BUILD_COMMAND = $(BASELOC)/cosmo-repo/cli/build-wrapper
 
+o/cosmo-repo/cli/built.aarch64: o/cosmo-repo/cli/built.x86_64
+
 o/cosmo-repo/cli/installed.x86_64: INSTALL_COMMAND = $(DUMMYLINK0)
 o/cosmo-repo/cli/installed.aarch64: INSTALL_COMMAND = $(DUMMYLINK0)
 
