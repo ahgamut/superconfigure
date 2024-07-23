0002a04dac38ddd886e3dda480e465e2208d242a
Gautham <ahgamut@gmail.com>
2024-07-22 23:12:42 -0500

    use gcc-14.1
    gcc-14.1 build uses the following github branch:
    
    https://github.com/ahgamut/gcc/tree/portcosmo-14.1
    
    build has not been tested yet.

--- compiler/BUILD.mk
+++ compiler/BUILD.mk
@@ -1,6 +1,6 @@
 
 include compiler/binutils/BUILD.mk
-include compiler/gcc-12.3-patched/BUILD.mk
+include compiler/gcc-14.1-patched/BUILD.mk
 include compiler/llvm/BUILD.mk
 
 cosmocc: x86_64-gcc aarch64-gcc
