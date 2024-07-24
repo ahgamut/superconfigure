68ad9d88e7593635813c08fffbad028b109bdad2
Gautham <ahgamut@gmail.com>
2024-07-24 12:14:46 +0000

    remove unbourne

--- cosmo-repo/cli/BUILD.mk
+++ cosmo-repo/cli/BUILD.mk
@@ -14,7 +14,7 @@ o/cosmo-repo/cli/installed.x86_64: INSTALL_COMMAND = $(DUMMYLINK0)
 o/cosmo-repo/cli/installed.aarch64: INSTALL_COMMAND = $(DUMMYLINK0)
 
 o/cosmo-repo/cli/built.fat: BINS = 	greenbean life nesemu1 script \
-	ttyinfo unbourne wall awk \
+	ttyinfo wall awk \
 	ctags lua make \
 	tree tidy assimilate sqlite3 \
 	mktemper cpuid \
