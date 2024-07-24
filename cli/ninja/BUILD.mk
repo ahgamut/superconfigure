9ce05a2915298eb9e317d85d660ae9325e3bccf6
Gautham <ahgamut@gmail.com>
2024-07-24 13:11:00 +0000

    get ninja and zsh to build

--- cli/ninja/BUILD.mk
+++ cli/ninja/BUILD.mk
@@ -2,6 +2,7 @@
 NINJA_SRC := https://github.com/ninja-build/ninja/archive/refs/tags/v1.11.1.tar.gz
 NINJA_CONFIG_ARGS = -DCMAKE_BUILD_SHARED=OFF\
     -DCMAKE_BUILD_STATIC=ON\
+	-DCMAKE_CXX_FLAGS="-std=c++20"\
     -DCMAKE_INSTALL_PREFIX="$$(COSMOS)"
 
 $(eval $(call DOWNLOAD_SOURCE,cli/ninja,$(NINJA_SRC)))
