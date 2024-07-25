ed48a8b46078d26c240d116cdcda1ba576c9d436
Gautham <ahgamut@gmail.com>
2024-07-25 18:02:33 +0000

    disable pigz build for now

--- compress/BUILD.mk
+++ compress/BUILD.mk
@@ -5,16 +5,13 @@ include compress/gzip/BUILD.mk
 include compress/lz4/BUILD.mk
 # include compress/lzlib/BUILD.mk
 # include compress/lzip/BUILD.mk
-include compress/pigz/BUILD.mk
+# include compress/pigz/BUILD.mk
 include compress/tar/BUILD.mk
 
 compress: \
 	o/cosmo-repo/compress/built.fat\
 	o/compress/brotli/built.fat\
 	o/compress/gzip/built.fat\
-	o/compress/pigz/built.fat\
 	o/compress/tar/built.fat
-
-# lzip build doesn't work for some reason
 
 .PHONY: compress
