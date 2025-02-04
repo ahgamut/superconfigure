5955f888397ff7ec2cb50b71cf0db7e03e5e6a48
rladaga <93140602+rladaga@users.noreply.github.com>
2025-02-04 14:52:00 -0300

    Removing extra patches and changing paths to ensure built.fat works correctly

--- games/nethack/BUILD.mk
+++ games/nethack/BUILD.mk
@@ -5,8 +5,8 @@ NETHACK_DEPS := lib/ncurses
 $(eval $(call DOWNLOAD_SOURCE,games/nethack,$(NETHACK_SRC)))
 $(eval $(call SPECIFY_DEPS,games/nethack,$(NETHACK_DEPS)))
 
-o/games/nethack/configured.x86_64: CONFIG_COMMAND = $(BASELOC)/games/nethack/config-wrapper minimal_x86.diff
-o/games/nethack/configured.aarch64: CONFIG_COMMAND = $(BASELOC)/games/nethack/config-wrapper minimal_aarch64.diff
+o/games/nethack/configured.x86_64: CONFIG_COMMAND = $(BASELOC)/games/nethack/config-wrapper
+o/games/nethack/configured.aarch64: CONFIG_COMMAND = $(BASELOC)/games/nethack/config-wrapper
 
 o/games/nethack/built.fat: FATTEN_COMMAND = $(BASELOC)/config/apelink_folder.sh
 o/games/nethack/built.fat: BINS = nethack share/terminfo sysconf nhdat
