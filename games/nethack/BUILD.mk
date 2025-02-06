516c8082843b829247034fe36f71d9bdbdc600d7
Gautham <ahgamut@gmail.com>
2025-02-06 05:23:10 +0000

    update nethack build to avoid home dir

--- games/nethack/BUILD.mk
+++ games/nethack/BUILD.mk
@@ -8,5 +8,10 @@ $(eval $(call SPECIFY_DEPS,games/nethack,$(NETHACK_DEPS)))
 o/games/nethack/configured.x86_64: CONFIG_COMMAND = $(BASELOC)/games/nethack/config-wrapper
 o/games/nethack/configured.aarch64: CONFIG_COMMAND = $(BASELOC)/games/nethack/config-wrapper
 
+o/games/nethack/installed.x86_64: \
+	INSTALL_COMMAND = $(BASELOC)/games/nethack/install-wrapper
+o/games/nethack/installed.aarch64: \
+	INSTALL_COMMAND = $(BASELOC)/games/nethack/install-wrapper
+
 o/games/nethack/built.fat: FATTEN_COMMAND = $(BASELOC)/config/apelink_folder.sh
-o/games/nethack/built.fat: BINS = nethack share/terminfo sysconf nhdat
+o/games/nethack/built.fat: BINS = nethack share/terminfo share/nh
