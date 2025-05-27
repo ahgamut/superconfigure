f422baf856afc02df9640710f1ae8b8609b6500e
Gautham <ahgamut@gmail.com>
2025-05-27 18:43:24 +0000

    add libarchive

--- compress/BUILD.mk
+++ compress/BUILD.mk
@@ -1,6 +1,7 @@
 
 include compress/brotli/BUILD.mk
 include compress/gzip/BUILD.mk
+include compress/libarchive/BUILD.mk
 include compress/lz4/BUILD.mk
 include compress/lzlib/BUILD.mk
 include compress/lzip/BUILD.mk
@@ -10,10 +11,11 @@ include compress/tar/BUILD.mk
 compress: \
 	o/cosmo-repo/compress/built.fat\
 	o/compress/brotli/built.fat\
-	o/compress/pigz/built.fat\
+	o/compress/gzip/built.fat\
+	o/compress/libarchive/built.fat\
 	o/compress/lzip/built.fat\
 	o/compress/lz4/built.fat\
-	o/compress/gzip/built.fat\
+	o/compress/pigz/built.fat\
 	o/compress/tar/built.fat
 
 .PHONY: compress
---
+++ compress/libarchive/BUILD.mk
@@ -0,0 +1,19 @@
+
+LIBARCHIVE_SRC := https://github.com/libarchive/libarchive/releases/download/v3.8.0/libarchive-3.8.0.tar.gz
+
+LIBARCHIVE_CONFIG_ARGS = -DCMAKE_INSTALL_PREFIX=$$(COSMOS)\
+	-DENABLE_ACL=OFF -DENABLE_XATTR=OFF\
+	-DENABLE_TEST=OFF -DBUILD_SHARED_LIBS=OFF\
+	-DENABLE_OPENSSL=OFF\
+    -DENABLE_CPIO_SHARED=OFF -DENABLE_CAT_SHARED=OFF -DENABLE_TAR_SHARED=OFF\
+	-DCMAKE_IGNORE_PREFIX_PATH="/usr/;"\
+    -DCMAKE_C_FLAGS="-Os"
+
+LIBARCHIVE_DEPS := cosmo-repo/base cosmo-repo/compress\
+	lib/libxml2 lib/libexpat lib/pcre compress/lz4
+
+$(eval $(call DOWNLOAD_SOURCE,compress/libarchive,$(LIBARCHIVE_SRC)))
+$(eval $(call SPECIFY_DEPS,compress/libarchive,$(LIBARCHIVE_DEPS)))
+$(eval $(call CMAKE_BUILD,compress/libarchive,$(LIBARCHIVE_CONFIG_ARGS),$(LIBARCHIVE_CONFIG_ARGS)))
+
+o/compress/libarchive/built.fat: BINS = bsdtar bsdcpio bsdcat bsdunzip
