6c71e813aa11bc87551812a34c699a393e42ce8a
Gautham <ahgamut@gmail.com>
2025-06-09 22:01:53 -0500

    remove libarchive from compress collection
    build flake -- collision between x86_64 and aarch64 builds

--- compress/BUILD.mk
+++ compress/BUILD.mk
@@ -12,7 +12,6 @@ compress: \
 	o/cosmo-repo/compress/built.fat\
 	o/compress/brotli/built.fat\
 	o/compress/gzip/built.fat\
-	o/compress/libarchive/built.fat\
 	o/compress/lzip/built.fat\
 	o/compress/lz4/built.fat\
 	o/compress/pigz/built.fat\
