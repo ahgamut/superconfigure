197fdf9c5204b07207d44b6fff88b433d01954ad
Gautham <ahgamut@gmail.com>
2025-06-09 22:21:15 -0500

    skip fatten for libsodium

--- lib/libsodium/BUILD.mk
+++ lib/libsodium/BUILD.mk
@@ -4,6 +4,7 @@ LIBSODIUM_CONFIG_ARGS = --without-libtool --disable-shared\
 	--disable-ssp --disable-asm --disable-pie\
     --prefix=$$(COSMOS) CFLAGS="-Os"
 
-
 $(eval $(call DOWNLOAD_SOURCE,lib/libsodium,$(LIBSODIUM_SRC)))
 $(eval $(call AUTOTOOLS_BUILD,lib/libsodium,$(LIBSODIUM_CONFIG_ARGS),$(LIBSODIUM_CONFIG_ARGS)))
+
+o/lib/libsodium/built.fat: FATTEN_COMMAND = $(DUMMYLINK0)
