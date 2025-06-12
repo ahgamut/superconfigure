5a68e499bd7ad232cb48348e630a1c34a47831c3
Gautham <ahgamut@gmail.com>
2025-06-12 19:10:41 +0000

    add config in separate step

--- cli/toybox/BUILD.mk
+++ cli/toybox/BUILD.mk
@@ -2,6 +2,12 @@ TOYBOX_SRC := https://landley.net/toybox/downloads/toybox-0.8.12.tar.gz
 
 $(eval $(call DOWNLOAD_SOURCE,cli/toybox,$(TOYBOX_SRC)))
 
+o/cli/toybox/setup: o/cli/toybox/patched
+	cp $(BASELOC)/cli/toybox/cosmo.config $(BASELOC)/o/cli/toybox/toybox-0.8.12/.config
+
+o/cli/toybox/configured.x86_64: o/cli/toybox/setup
+o/cli/toybox/configured.aarch64: o/cli/toybox/setup
+
 o/cli/toybox/configured.x86_64: CONFIG_COMMAND = $(BASELOC)/cli/toybox/config-wrapper
 o/cli/toybox/built.x86_64: BUILD_COMMAND = make V=1 toybox
 o/cli/toybox/installed.x86_64: INSTALL_COMMAND = cp toybox $(COSMOS)/bin/toybox
