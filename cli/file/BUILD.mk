e0b4a0db50e64c4823c57994483f735738ff09f1
Gautham <ahgamut@gmail.com>
2026-01-09 19:05:44 +0000

    simplify file build config

--- cli/file/BUILD.mk
+++ cli/file/BUILD.mk
@@ -2,18 +2,16 @@ FILE_SRC := https://github.com/file/file/archive/refs/tags/FILE5_46.tar.gz
 
 $(eval $(call DOWNLOAD_SOURCE,cli/file,$(FILE_SRC)))
 
-o/cli/file/setup:
+o/cli/file/setup: o/cli/file/patched
 	cd $(BASELOC)/o/cli/file/file-* && autoreconf -fi $(ERRLOG) && autoconf $(ERRLOG)
 	touch $@
 
 o/cli/file/configured.x86_64: o/cli/file/setup
 o/cli/file/configured.aarch64: o/cli/file/setup
 
-o/cli/file/configured.x86_64: CONFIG_COMMAND = $(BASELOC)/cli/file/config-wrapper
-o/cli/file/configured.aarch64: CONFIG_COMMAND = $(BASELOC)/cli/file/config-wrapper
+FILE_CONFIG_ARGS = --disable-shared --prefix=$$(COSMOS)
 
-o/cli/file/installed.x86_64: INSTALL_COMMAND = $(BASELOC)/cli/file/install-wrapper
-o/cli/file/installed.aarch64: INSTALL_COMMAND = $(BASELOC)/cli/file/install-wrapper
+$(eval $(call AUTOTOOLS_BUILD,cli/file,$(FILE_CONFIG_ARGS),$(FILE_CONFIG_ARGS)))
 
 o/cli/file/built.fat: FATTEN_COMMAND = $(BASELOC)/config/apelink_folder.sh
 o/cli/file/built.fat: BINS = file share/misc
