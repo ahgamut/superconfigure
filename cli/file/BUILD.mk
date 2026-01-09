b3cd977af3d312a7c1d94846d3b3687e4a408d94
Brett Jia <dev.bjia56@gmail.com>
2026-01-09 13:53:05 -0500

    cleanup file build
    * file: fix configure script generation
    
    * add setup target

--- cli/file/BUILD.mk
+++ cli/file/BUILD.mk
@@ -2,7 +2,12 @@ FILE_SRC := https://github.com/file/file/archive/refs/tags/FILE5_46.tar.gz
 
 $(eval $(call DOWNLOAD_SOURCE,cli/file,$(FILE_SRC)))
 
-o/cli/file/patched: PATCH_COMMAND = $(DUMMYLINK0)
+o/cli/file/setup:
+	cd $(BASELOC)/o/cli/file/file-* && autoreconf -fi $(ERRLOG) && autoconf $(ERRLOG)
+	touch $@
+
+o/cli/file/configured.x86_64: o/cli/file/setup
+o/cli/file/configured.aarch64: o/cli/file/setup
 
 o/cli/file/configured.x86_64: CONFIG_COMMAND = $(BASELOC)/cli/file/config-wrapper
 o/cli/file/configured.aarch64: CONFIG_COMMAND = $(BASELOC)/cli/file/config-wrapper
