cfc56c70e23d682c31743d469a65b160a1a77e46
Gautham Venkatasubramanian <ahgamut@gmail.com>
2026-05-23 20:02:12 -0500

    skip berry build

--- lang/BUILD.mk
+++ lang/BUILD.mk
@@ -1,5 +1,4 @@
 
-include lang/berry/BUILD.mk
 include lang/janet/BUILD.mk
 include lang/lua/BUILD.mk
 include lang/php/BUILD.mk
@@ -7,7 +6,6 @@ include lang/python/BUILD.mk
 include lang/tcl/BUILD.mk
 
 lang: \
-	o/lang/berry/built.fat \
 	o/lang/janet/built.fat \
 	o/lang/lua/built.fat \
 	o/lang/php/built.fat \
